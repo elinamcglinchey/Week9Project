395569da1873034266d22c2ab1c8fe56f6286067
elinamcglinchey <elinamcglinchey1@gmail.com>
2022-06-29 11:02:43 +0000

    test app imports

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from flask import url_for
 from flask_testing import TestCase
 
+# forms not needed
 from application import app, db
 from application.models import Memberships, MealPlans
 from flask import redirect, url_for, render_template, request
